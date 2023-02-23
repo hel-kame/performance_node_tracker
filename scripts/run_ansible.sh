bfa32159686dc5cf08f0d976e95c030e35db0bb1
creat0xr <0xrequiem1@gmail.com>
2023-02-23 23:34:28 +0100

    add(juno_env)

--- scripts/run_ansible.sh
+++ scripts/run_ansible.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir ~/.playbooks
-cp /vagrant/playbooks/installation_playbook.yml ~/.playbooks/
-ansible-playbook ~/.playbooks/installation_playbook.yml
+cp /vagrant/playbooks/juno_playbook.yml ~/.playbooks/
+ansible-playbook ~/.playbooks/juno_playbook.yml
