53b1505f367caaac50f5f5dfd8f51f18ba062e3c
Hithem EL KAMEL <hel-kame@e1r12p3.clusters.42paris.fr>
2023-02-23 18:01:51 +0100

    add(data)

--- tracker/tracker.sh
+++ tracker/tracker.sh
@@ -20,7 +20,7 @@ fi
 
 if [ ! -f "tracker.log" ];
 then
-	echo -e "\t\t%CPU\t%MEM\tkb_RD/s\tkb_WR/s\tPID\tPROCESS" > tracker.log
+	echo -e "\t\t%CPU\t%MEM\tkb_RD/s\t\tkb_WR/s\t\tSNT/s\t\tRCD/s\t\tPID\tPROCESS" > tracker.log
 fi
 
 if date | grep "PM" > /dev/null
@@ -35,9 +35,29 @@ else
 	RD_USAGE=$(pidstat -d -C ${PNAME} | awk 'NR == 4 {print $4}')
 	WR_USAGE=$(pidstat -d -C ${PNAME} | awk 'NR == 4 {print $5}')
 fi
-	TOTAL_CPU=$(tail tracker.log -n +2 | awk '{print $3}' | paste -sd+ | bc)
-	AVERAGE_CPU=$(echo ${TOTAL_CPU} / $(tail tracker.log -n +2 | wc -l) | bc)
 
-echo -e "$(date +%X)\t${CPU_USAGE}\t${RAM_USAGE}\t${RD_USAGE}\t${WR_USAGE}\t$(pidof ${PNAME})\t${PNAME}" >> tracker.log
-echo -e "$(date +%X)\t${CPU_USAGE}\t${RAM_USAGE}\t${RD_USAGE}\t${WR_USAGE}\t$(pidof ${PNAME})\t${PNAME}" >> tracker.log
+if [ ! -f "data.tmp" ]
+then
+	sudo nethogs -t -c 2  > data.tmp
+fi
+
+if cat data.tmp | grep ${PNAME} > /dev/null
+then
+	DATA_SENT=$(cat data.tmp | grep ${PNAME} | awk '{print $2}')
+	DATA_RECEIVED=$(cat data.tmp | grep ${PNAME} | awk '{print $3}')
+	rm data.tmp
+else
+	DATA_SENT=$(echo -n "0")
+	DATA_RECEIVED=$(echo -n "0")
+	rm data.tmp
+fi
+
+round() {
+  printf "%.${2}f" "${1}"
+}
+
+ROUND_DATA_SENT=$(round ${DATA_SENT} 2)
+ROUND_DATA_RECEIVED=$(round ${DATA_RECEIVED} 2)
+
+echo -e "$(date +%X)\t${CPU_USAGE}\t${RAM_USAGE}\t${RD_USAGE}\t\t${WR_USAGE}\t\t${ROUND_DATA_SENT}\t\t${ROUND_DATA_RECEIVED}\t\t$(pidof ${PNAME})\t${PNAME}" >> tracker.log
 cat tracker.log
